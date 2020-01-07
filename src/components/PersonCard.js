93930b6769c008c01e2381e1885368e1159aaa2e
Mark Albiston <mark.albiston@gmail.com>
2020-01-07 13:57:13 -0700

    putting it together with birthday button

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            birthday: props.age
+        }
+    }
+    handleClick = () => {
+        this.setState({
+            birthday: this.state.birthday + 1
+        })
+    }
     render(){
-        const {firstName, lastName, age, hairColor} = this.props;
+        const {firstName, lastName, hairColor} = this.props;
         return(
             <div>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.birthday}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={this.handleClick}> Button for {firstName} {lastName}'s birthday</button>
             </div>
         )
     }
